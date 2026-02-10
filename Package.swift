b844e8978532c90278196030af6418da5b601c2d
netomi-sdk-admin <sdk-admin@netomi.com>
2026-02-10 06:29:44 +0000

    chore(release): v1.18.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Netomi",
             url: "https://netomi-sdk-public.s3.amazonaws.com/sdk/ios/releases/1.18.0/Netomi.xcframework.zip",
-            checksum: "e6c1bf52561b34fe086e7b0aaa0a6164670de703262dd97b15fa234aeb01aa24"
+            checksum: "c5f028fff6766de6e4af31b46a0da102f555c1b19b058ac9e7ac0d038b27f7c2"
         )
     ]
 )
